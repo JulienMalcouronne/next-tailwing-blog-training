e765d34c95c32f7ef221e82b1dd6fd794278c073
julien malcouronne <malcouronnejulien@gmail.com>
2024-04-09 19:56:36 +0200

    wip: wip

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,6 +1,7 @@
 import PaddingContainer from '@/components/layout/padding-container';
 import PostList from '@/components/post/post-list/post-list';
 import { DUMMY_CATEGORIES, DUMMY_POSTS } from '@/mocks';
+import { prisma } from '../db';
 
 export const generateStaticParams = async () => {
   return DUMMY_CATEGORIES.map((category) => {
@@ -9,20 +10,37 @@ export const generateStaticParams = async () => {
     };
   });
 };
+const getCategoryData = async (slug: string) => {
+  try {
+    return await prisma.categories.findFirst({
+      where: {
+        slug,
+      },
+      include: {
+        Posts,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const Page = ({
+const Page = async ({
   params,
 }: {
   params: {
     category: string;
   };
 }) => {
-  const category = DUMMY_CATEGORIES.find(
-    (category) => category.slug === params.category,
-  );
-  const posts = DUMMY_POSTS.filter(
-    (post) => post.category.title.toLocaleLowerCase() === params.category,
-  );
+  const category = await getCategoryData(params.category);
+  console.log(category);
+  // const category = DUMMY_CATEGORIES.find(
+  //   (category) => category.slug === params.category,
+  // );
+  // const posts = DUMMY_POSTS.filter(
+  //   (post) => post.category.title.toLocaleLowerCase() === params.category,
+  // );
+
   return (
     <PaddingContainer>
       <div className="mb-10">
@@ -30,7 +48,7 @@ const Page = ({
         <p className="text-lg text-neutral-600">{category?.description}</p>
       </div>
 
-      <PostList posts={posts} />
+      {/* <PostList posts={posts} /> */}
     </PaddingContainer>
   );
 };
